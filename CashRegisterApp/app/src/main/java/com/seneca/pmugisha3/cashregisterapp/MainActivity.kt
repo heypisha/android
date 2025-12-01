0bd69f86629d89e1ca418ad0647273673afe39b8
Pierrette Mugisha <hey.pisha@gmail.com>
2025-11-30 21:48:40 -0500

    CashRegisterApp - initial commit

---
+++ CashRegisterApp/app/src/main/java/com/seneca/pmugisha3/cashregisterapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.seneca.pmugisha3.cashregisterapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.seneca.pmugisha3.cashregisterapp.ui.theme.CashRegisterAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CashRegisterAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CashRegisterAppTheme {
+        Greeting("Android")
+    }
+}
